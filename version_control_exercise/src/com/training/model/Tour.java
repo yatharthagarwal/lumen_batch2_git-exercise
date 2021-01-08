aca662a5bdfeb787196bb3ec45fa25f0da7435b4
Yatharth <yatharthagrawal@gmail.com>
2021-01-08 12:41:59 +0530

    REQ-102 Added hashcode and equals

--- version_control_exercise/src/com/training/model/Tour.java
+++ version_control_exercise/src/com/training/model/Tour.java
@@ -42,10 +42,41 @@ public class Tour {
 			this.cost = cost;
 		}
 		@Override
-		public String toString() {
-			return "Tour [tourId=" + tourId + ", tourName=" + tourName + ", duration=" + duration + ", cost=" + cost
-					+ "]";
+		public int hashCode() {
+			final int prime = 31;
+			int result = 1;
+			long temp;
+			temp = Double.doubleToLongBits(cost);
+			result = prime * result + (int) (temp ^ (temp >>> 32));
+			temp = Double.doubleToLongBits(duration);
+			result = prime * result + (int) (temp ^ (temp >>> 32));
+			result = prime * result + tourId;
+			result = prime * result + ((tourName == null) ? 0 : tourName.hashCode());
+			return result;
 		}
+		@Override
+		public boolean equals(Object obj) {
+			if (this == obj)
+				return true;
+			if (obj == null)
+				return false;
+			if (getClass() != obj.getClass())
+				return false;
+			Tour other = (Tour) obj;
+			if (Double.doubleToLongBits(cost) != Double.doubleToLongBits(other.cost))
+				return false;
+			if (Double.doubleToLongBits(duration) != Double.doubleToLongBits(other.duration))
+				return false;
+			if (tourId != other.tourId)
+				return false;
+			if (tourName == null) {
+				if (other.tourName != null)
+					return false;
+			} else if (!tourName.equals(other.tourName))
+				return false;
+			return true;
+		}
+		
 
 		
 }
